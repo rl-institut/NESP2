1c22a617c692bacd6ae3ff47df44723163685739
christian-rli <christian.hofmann@rl-institut.de>
2019-11-20 18:33:25 +0100

    Make buttons show level-specific layers #10

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -1,3 +1,36 @@
+function national_button_fun() {
+  var hidelist = document.getElementsByClassName("n_hide");
+  var showlist = document.getElementsByClassName("n_show");
+  for (i = 0; i < hidelist.length; i++) {
+    hidelist[i].style.display = "none";
+  }
+  for (j = 0; j < showlist.length; j++) {
+    showlist[j].style.display = "block";
+  }
+}
+
+function state_button_fun() {
+  var hidelist = document.getElementsByClassName("s_hide");
+  var showlist = document.getElementsByClassName("s_show");
+  for (i = 0; i < hidelist.length; i++) {
+    hidelist[i].style.display = "none";
+  }
+  for (j = 0; j < showlist.length; j++) {
+    showlist[j].style.display = "block";
+  }
+}
+
+function village_button_fun() {
+  var hidelist = document.getElementsByClassName("v_hide");
+  var showlist = document.getElementsByClassName("v_show");
+  for (i = 0; i < hidelist.length; i++) {
+    hidelist[i].style.display = "none";
+  }
+  for (j = 0; j < showlist.length; j++) {
+    showlist[j].style.display = "block";
+  }
+}
+
 function states_cb_fun() {
   var checkBox = document.getElementById("states_cb");
   var text = document.getElementsByClassName("substate");
