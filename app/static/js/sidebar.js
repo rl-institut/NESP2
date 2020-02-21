eb013874e4bad48dfe4828cfb055cfd72c21ab44
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-02-21 08:45:23 +0100

    Add state in the download link

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -511,7 +511,8 @@ function buildingDensity_cb_fun() {
 
 function download_clusters_fun() {
     var export_csv_link = document.getElementById("export_csv")
-    export_csv_link.href="/csv-export?min_area=" + currentfilter.minarea + ";max_area=" + currentfilter.maxarea
+    export_csv_link.href="/csv-export?state="+selectedState+"&min_area=" + currentfilter.minarea +
+    "&max_area=" + currentfilter.maxarea
     export_csv_link.click()
 }
 
