a749ef6f522064fc09384e455ed24ae437d114ab
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-02-28 20:27:33 +0100

    Rework url for get method

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -529,42 +529,29 @@ function download_clusters_fun() {
   // currently there are only two filters which are mutually exclusive
   if (checkBox.checked == true){
 
-         $.post({
-        url: "/csv-export",
-        dataType: "json",
-        data: {
-            "cluster_type": "cluster",
-            "state_name": selectedState,
-            "area": [currentfilter.minarea, currentfilter.maxarea],
-            "distance_grid": [currentfilter.mindtg, currentfilter.maxdtg],
-            },
-
-        success: function(data){console.log(data);},
-     }).done(function() {console.log("now done");});
-
-  // TODO send the correct filter depending on og cluster or populated areas
-    export_csv_link.href = "/csv-export?state=" + selectedState + "&min_area=" + currentfilter.minarea +
-    "&max_area=" + currentfilter.maxarea
+    export_csv_link.href = "/csv-export"
+    + "?state=" + selectedState
+    + "&cluster_type=cluster"
+    + "&min_area=" + currentfilter.minarea
+    + "&max_area=" + currentfilter.maxarea
+    + "&mindtg=" + currentfilter.mindtg
+    + "&maxdtg=" + currentfilter.maxdtg
   }
   else{
-    $.post({
-        url: "/csv-export",
-        dataType: "json",
-        data: JSON.stringify({
-            "cluster_type": "ogcluster",
-            "state_name": selectedState,
-            "area": [currentfilter.ogminarea, currentfilter.ogmaxarea],
-            "distance_grid": [currentfilter.ogmindtg, currentfilter.ogmaxdtg],
-            "buildings": [currentfilter.ogminb, currentfilter.ogmaxb],
-            "buildings_footprint": [currentfilter.ogminbfp, currentfilter.ogmaxbfp],
-            }),
-        success: function(data){console.log(data);},
-     }).done(function() {console.log("now done");});
-  // TODO send the correct filter depending on og cluster or populated areas
-      export_csv_link.href = "/csv-export?state=" + selectedState + "&min_area=" + currentfilter.minarea +
-    "&max_area=" + currentfilter.maxarea
+
+    export_csv_link.href = "/csv-export"
+    + "?state=" + selectedState
+    + "&cluster_type=ogcluster"
+    + "&ogmin_area=" + currentfilter.ogminarea
+    + "&ogmax_area=" + currentfilter.ogmaxarea
+    + "&ogmindtg=" + currentfilter.ogmindtg
+    + "&ogmaxdtg=" + currentfilter.ogmaxdtg
+    + "&ogminb=" + currentfilter.ogminb
+    + "&ogmaxb=" + currentfilter.ogmaxb
+    + "&ogminbfp=" + currentfilter.ogminbfp
+    + "&ogmaxbfp=" + currentfilter.ogmaxbfp
   }
-  //export_csv_link.click()
+  export_csv_link.click()
 }
 
 function clusters_cb_fun() {
