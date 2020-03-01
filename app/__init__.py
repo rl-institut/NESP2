ec010d1b4aadcac067be9964af157e031eea6ca6
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-03-01 22:15:46 +0100

    Adapt filename and fields depending on cluster types

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 import os
-import json
 from flask import Flask, render_template, request, jsonify, url_for, redirect, Response
 from flask_wtf.csrf import CSRFProtect
 from .utils import define_function_jinja
@@ -63,24 +62,37 @@ def create_app(test_config=None):
         args = request.args
         state = args.get("state")
         cluster_type = args.get("cluster_type")
+        fname = args.get("state")
+
         if os.environ.get("POSTGRES_URL", None) is not None:
-            keys = (
-                'adm1_pcode',
-                'cluster_offgrid_id',
-                'area_km2',
-                'building_count',
-                'percentage_building_area',
-                'grid_dist_km'
-            )
             if "og" in cluster_type:
+                fname = fname + "_remotely_mapped_settlements"
+                keys = (
+                    'adm1_pcode',
+                    'cluster_offgrid_id',
+                    'area_km2',
+                    'building_count',
+                    'percentage_building_area',
+                    'grid_dist_km'
+                )
                 records = query_filtered_og_clusters(
                     state,
                     STATE_CODES_DICT,
                     area=[args.get("ogmin_area"), args.get("ogmax_area")],
                     distance_grid=[args.get("ogmindtg"), args.get("ogmaxdtg")],
+                    building=[args.get("ogminb"), args.get("ogmaxb")],
+                    buildingfp=[args.get("ogminbfp"), args.get("ogmaxbfp")],
                     keys=keys
                 )
             else:
+                fname = fname + "_identified_settlements_by_satellite"
+                keys = (
+                    'adm1_pcode',
+                    'cluster_all_id',
+                    'fid',
+                    'area_km2',
+                    'grid_dist_km'
+                )
                 records = query_filtered_clusters(
                     state,
                     STATE_CODES_DICT,
@@ -89,18 +101,17 @@ def create_app(test_config=None):
                     keys=keys
                 )
 
-            csv = []
+            csv = list()
             csv.append(", ".join(keys))
             for line in records:
                 csv.append(", ".join([str(line[k]) for k in keys]))
             csv = "\n".join(csv) + "\n"
         else:
-            csv = "1,2,3\n,5,6\n"
-        print(csv)
+            csv = "1,2,3\n4,5,6\n"
         return Response(
             csv,
             mimetype="text/csv",
-            headers={"Content-disposition": "attachment; filename={}.csv".format(args.get("state"))}
+            headers={"Content-disposition": "attachment; filename={}.csv".format(fname)}
         )
 
     @app.route('/filter-cluster', methods=["POST"])
