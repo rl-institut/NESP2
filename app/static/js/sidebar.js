d41badfffa1b29387ac2b1b9dafa32d21617a466
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-11-04 23:53:54 +0100

    Change filter boundaries in filter_settlements_ids

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -1237,11 +1237,11 @@ function filter_settlements_ids(){
       total_clusters = total_clusters + 1;
       if (
         centroids[key].feature.properties.grid_dist_km >= currentfilter.ogmindtg && 
-        centroids[key].feature.properties.grid_dist_km < currentfilter.ogmaxdtg &&
+        centroids[key].feature.properties.grid_dist_km <= currentfilter.ogmaxdtg &&
         centroids[key].feature.properties.building_count >= currentfilter.ogminb &&
-        centroids[key].feature.properties.building_count < currentfilter.ogmaxb &&
+        centroids[key].feature.properties.building_count <= currentfilter.ogmaxb &&
         centroids[key].feature.properties.percentage_building_area >= currentfilter.ogminbfp &&
-        centroids[key].feature.properties.percentage_building_area < currentfilter.ogmaxbfp
+        centroids[key].feature.properties.percentage_building_area <= currentfilter.ogmaxbfp
       ){
         filtered_centroids_keys.push({"key": key, "area": centroids[key].feature.properties.building_count});
         og_centroids_dict[centroids[key].feature.properties.cluster_offgrid_id] = key;
@@ -1251,9 +1251,9 @@ function filter_settlements_ids(){
       total_clusters = total_clusters + 1;
       if (
         centroids[key].feature.properties.area_km2 >= currentfilter.minarea &&
-        centroids[key].feature.properties.area_km2 < currentfilter.maxarea &&
+        centroids[key].feature.properties.area_km2 <= currentfilter.maxarea &&
         centroids[key].feature.properties.grid_dist_km >= currentfilter.mindtg &&
-        centroids[key].feature.properties.grid_dist_km < currentfilter.maxdtg
+        centroids[key].feature.properties.grid_dist_km <= currentfilter.maxdtg
       ){
         filtered_centroids_keys.push({"key": key, "area": centroids[key].feature.properties.area_km2});
         all_centroids_dict[centroids[key].feature.properties.cluster_all_id] = key;
